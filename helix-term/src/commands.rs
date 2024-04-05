3bb1119552045cc4a22070a84fc6bf339c1138bc
Michael Davis <mcarsondavis@gmail.com>
2024-04-05 14:49:02 -0400

    Avoid allocations in Picker file preview callback
    The `FileLocation` and `PathOrId` types can borrow paths rather than
    requiring them to be owned. This takes a refactor of the preview
    functions and preview internals within `Picker`. With this change we
    avoid an unnecessary `PathBuf` clone per render for any picker with a
    file preview function (i.e. most pickers).
    
    This refactor is not fully complete. The `PathOrId` is _sometimes_ an
    owned `PathBuf`. This is for pragmatic reasons rather than technical
    ones. We need a further refactor to introduce more core types like
    `Location` in order to eliminate the Cow and only use `&Path`s within
    `PathOrId`. This is left for future work as it will be a larger refactor
    almost entirely fitting into the LSP commands module and helix-core -
    i.e. mostly unrelated to refactoring the `Picker` code itself.
    
    Co-authored-by: Pascal Kuthe <pascalkuthe@pm.me>

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -2436,7 +2436,7 @@ fn global_search(cx: &mut Context) {
         },
     )
     .with_preview(|_editor, FileResult { path, line_num, .. }| {
-        Some((path.clone().into(), Some((*line_num, *line_num))))
+        Some((path.as_path().into(), Some((*line_num, *line_num))))
     })
     .with_history_register(Some(reg))
     .with_dynamic_query(get_files, Some(275));
@@ -3099,7 +3099,7 @@ fn changed_file_picker(cx: &mut Context) {
             }
         },
     )
-    .with_preview(|_editor, meta| Some((meta.path().to_path_buf().into(), None)));
+    .with_preview(|_editor, meta| Some((meta.path().into(), None)));
     let injector = picker.injector();
 
     cx.editor
